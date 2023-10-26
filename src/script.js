4be61d297e9ff727cf8ae534ac2740d92984222e
JustinoBoggio <justinoboggio@gmail.com>
2023-10-26 18:10:41 -0300

    first version of the index page and the new vid page

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// Función para obtener la fecha actual en formato DD/MM/YYYY
+function getCurrentDate() {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1; // Los meses comienzan desde 0
+    const year = today.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+// Actualizar el contenido del elemento con el ID "current-date" con la fecha actual
+document.getElementById('current-date').innerText = getCurrentDate();
